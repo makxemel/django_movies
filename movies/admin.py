cc43f56b7c14e403e63506508e6d0266a3bad5ed
makx <makxemelianov@gmail.com>
2022-12-16 15:02:16 +0300

    added main page and tested display

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Actor)
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(MovieShot)
+admin.site.register(RaitingStar)
+admin.site.register(Raiting)
+admin.site.register(Reviews)
