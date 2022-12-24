01f9418b3ccd407b48acc816490eaf3e03b51774
makx <makxemelianov@gmail.com>
2022-12-24 20:00:34 +0300

    added ckeditor to the movie detail page

--- django_movies/urls.py
+++ django_movies/urls.py
@@ -21,8 +21,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('movies.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
